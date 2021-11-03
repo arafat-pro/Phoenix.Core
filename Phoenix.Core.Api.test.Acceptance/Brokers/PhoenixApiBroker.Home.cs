dff4765aabc1f935f1f85655b365ebb00b374fdb
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2021-11-03 20:17:46 +0600

    INFRA: Initialize Acceptance Test

---
+++ Phoenix.Core.Api.test.Acceptance/Brokers/PhoenixApiBroker.Home.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoenix.Core.Api.test.Acceptance.Brokers
+{
+    public partial class PhoenixApiBroker
+    {
+        private const string HomeRelativeUrl = "api/home";
+        public async ValueTask<string> GetHomeMessageAsync() =>
+            await this.apiFactoryClient.GetContentStringAsync(HomeRelativeUrl);
+    }
+}
---
+++ Phoenix.Core.Api.test.Acceptance/Brokers/PhoenixApiBroker.cs
@@ -0,0 +1,30 @@
+﻿// ------------------------------------------------
+// Copyright (c) Coalitions of Inspired Developers
+// FREE TO USE FOR THE WORLD
+// ------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc.Testing;
+using RESTFulSense.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoenix.Core.Api.test.Acceptance.Brokers
+{
+    public partial class PhoenixApiBroker
+    {
+        private readonly WebApplicationFactory<Startup> webApplicationFactory;
+        private readonly HttpClient httpClient;
+        private readonly IRESTFulApiFactoryClient apiFactoryClient;
+
+        public PhoenixApiBroker()
+        {
+            this.webApplicationFactory = new WebApplicationFactory<Startup>();
+            this.httpClient = this.webApplicationFactory.CreateClient();
+            this.apiFactoryClient = new RESTFulApiFactoryClient(this.httpClient);
+        }
+    }
+}
