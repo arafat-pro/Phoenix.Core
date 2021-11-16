1b6365320eeac494b1c91e1b1a665b76d59750d8
MD YASIR ARAFAT <ping@arafat.pro>
2021-11-17 01:44:58 +0600

    INFRA: Provision Project Initialization as Barebone

---
+++ Phoenix.Core.Api.Infrastructure.Provision/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phoenix.Core.Api.Infrastructure.Provision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
