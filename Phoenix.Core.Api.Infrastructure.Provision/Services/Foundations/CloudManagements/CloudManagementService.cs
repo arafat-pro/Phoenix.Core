4247d34b97da158c167c22e32e86d9875fa3495c
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2021-12-07 21:58:46 +0600

    FOUNDATIONS: Implement Provisioning Resource Group

---
+++ Phoenix.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Phoenix.Core.Api.Infrastructure.Provision.Brokers.Clouds;
+using Phoenix.Core.Api.Infrastructure.Provision.Brokers.Loggings;
+
+namespace Phoenix.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService(ICloudBroker cloudBroker, ILoggingBroker loggingBroker)
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public async ValueTask<IResourceGroup> ProvisionResourceGroupAsync(string projectName, string environment)
+        {
+            string resourceGroupName = $"{projectName}-RESOURCES-{environment}".ToUpper();
+            this.loggingBroker.LogActivity(message: $"Provisioning {resourceGroupName}...");
+
+            IResourceGroup resourceGroup = await this.cloudBroker.CreateResourceGroupAsync(resourceGroupName);
+
+            this.loggingBroker.LogActivity(message: $"{resourceGroupName} Provisioned.");
+
+            return resourceGroup;
+        }
+    }
+}
---
+++ Phoenix.Core.Api.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace Phoenix.Core.Api.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public interface ICloudManagementService
+    {
+        ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment);
+    }
+}
