062dc6e43ebc6188bcc398e28b4fdc26a5655f8a
MD YASIR ARAFAT <icsm.arafat@gmail.com>
2021-12-08 20:49:56 +0600

    PROCESSING: Provision

---
+++ Phoenix.Core.Api.Infrastructure.Provision/Services/Processings/CloudManagements/ICloudManagementProcessingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoenix.Core.Api.Infrastructure.Provision.Services.Processings.CloudManagements
+{
+    public interface ICloudManagementProcessingService
+    {
+        ValueTask ProcessAsync();
+    }
+}
